75ead1d96e45f7b83eefbb75ef3912a95b4fad0d
majid imanzade <majidimanzade1@gmail.com>
2020-02-23 22:14:25 +0330

    minor bug fix on test

--- spec/saoshyant_test.rb
+++ spec/saoshyant_test.rb
@@ -1,13 +1,14 @@
 require 'spec_helper'
+include Saoshyant
 
 describe Saoshyant do
 	describe 'JsonExceptionHandeler' do
 		it 'expect get error for invalid code status' do
-			expect{ Saoshyant::saoshyant_option(NameError, '200', false) }.to raise_error(RuntimeError)
+			expect{ saoshyant_option(NameError, '200', false) }.to raise_error(RuntimeError)
 		end
 
 		it 'expect get error for invalid log status' do
-			expect{ Saoshyant::saoshyant_option(NameError, 203, 'true') }.to raise_error(RuntimeError)
+			expect{ saoshyant_option(NameError, 203, 'true') }.to raise_error(RuntimeError)
 		end
 	end
 end
